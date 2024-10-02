1af57c7901d3269e7d0c7d30d5031670f0086dfc
ozn11 <skinnybaby79@gmail.com>
2024-10-02 20:09:30 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
 global.ownernumber = '393533870586'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownername = 'Cagula vpn👑' //owner name
 global.botname = '𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻3' //name of the bot
 
 //sticker details
